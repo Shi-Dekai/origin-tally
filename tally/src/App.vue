31a2ee2ff382f06185f6d15eeb61f5b804ba6a56
shi <1727259503@qq.com>
2020-07-04 16:03:24 +0800

    添加全局属性和全局变量

--- tally/src/App.vue
+++ tally/src/App.vue
@@ -1,41 +1,17 @@
 <template>
-  <div>
+  <div id="app">
     <router-view/>
   </div>
 </template>
 
 <style lang="scss">
-  * {
-    box-sizing: border-box;
-    padding: 0;
-    margin: 0;
-  }
+  @import "~@/assets/style/reset.scss";
+  @import "~@/assets/style/helper.scss";
   body{
-    line-height: 1.5;
-  }
-  a{
-    text-decoration: none;
-    color: inherit;
-  }
-
-  #app {
-    font-family: Avenir, Helvetica, Arial, sans-serif;
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
-    text-align: center;
-    color: #2c3e50;
-  }
-
-  #nav {
-    padding: 30px;
-
-    a {
-      font-weight: bold;
-      color: #2c3e50;
-
-      &.router-link-exact-active {
-        color: #42b983;
-      }
-    }
+    font-family: $font-hei;
+    line-height: 1.5;
+    color: #333;
   }
 </style>
