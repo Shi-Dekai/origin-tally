5d67ddc0942e481b2ff679224cccc9d9998c3369
shi <1727259503@qq.com>
2020-07-08 15:04:55 +0800

    可以选中或取消 tag

--- tally/src/components/Money/Tags.vue
+++ tally/src/components/Money/Tags.vue
@@ -4,45 +4,32 @@
       <button>新增标签</button>
     </div>
     <ul class="current">
-      <li>衣11</li>
-      <li>食22</li>
-      <li>住</li>
-      <li>行</li>
-      <li>衣</li>
-      <li>食</li>
-      <li>住</li>
-      <li>行</li>
-      <li>衣</li>
-      <li>食</li>
-      <li>住</li>
-      <li>行</li>
-      <li>衣</li>
-      <li>食</li>
-      <li>住</li>
-      <li>行</li>
-      <li>衣</li>
-      <li>食</li>
-      <li>住</li>
-      <li>行</li>
-      <li>衣</li>
-      <li>食</li>
-      <li>住</li>
-      <li>行</li>
-      <li>衣</li>
-      <li>食</li>
-      <li>住</li>
-      <li>行</li>
-      <li>衣</li>
-      <li>食</li>
-      <li>住33</li>
-      <li>行44</li>
+      <li v-for="tag in dataSource"
+          @click="toggle(tag)"
+          :class="{select:selectedTags.indexOf(tag)>=0}">{{tag}}
+      </li>
     </ul>
   </div>
 </template>
 
 <script lang="ts">
-  export default {
-    name: 'Tages'
+  import Vue from 'vue';
+  import {Component, Prop} from 'vue-property-decorator';
+
+  @Component
+  export default class Tags extends Vue {
+    @Prop() dataSource: string[] | undefined;
+    selectedTags: string[] = [];
+
+    toggle(tag: string){
+      const index = this.selectedTags.indexOf(tag)
+      if (index>=0){
+        this.selectedTags.splice(index, 1)
+      }else{
+        this.selectedTags.push(tag)
+      }
+    }
+
   };
 </script>
 
@@ -52,19 +39,26 @@
     flex-grow: 1;
     display: flex;
     flex-direction: column-reverse;
+
     > .current {
       display: flex;
       flex-wrap: wrap;
       /*justify-content: center;*/
       > li {
+        $bg: #d9d9d9;
         margin-top: 4px;
         $h: 24px;
-        background: #d9d9d9;
+        background: $bg;
         height: $h;
         line-height: $h;
         border-radius: ($h/2);
         padding: 0 16px;
         margin-right: 12px;
+
+        &.select {
+          background: darken($bg, 50%);
+          color: white;
+        }
       }
     }
 
--- tally/src/views/Money.vue
+++ tally/src/views/Money.vue
@@ -1,24 +1,30 @@
 <template>
-    <Layout class-prefix="layout">
-      <NumberPad />
-      <Types :xxx="333"/>
-      <Notes />
-      <Tags />
-    </Layout>
+  <Layout class-prefix="layout">
+    <NumberPad/>
+    <Types/>
+    <Notes/>
+    <Tags :data-source="tags"/>
+  </Layout>
 </template>
 
-<script lang="ts">
-  import NumberPad from '@/components/Money/NumberPad.vue';
-  import Types from '@/components/Money/Types.vue';
-  import Notes from '@/components/Money/Notes.vue';
-  import Tags from '@/components/Money/Tags.vue';
+<script>
+  import NumberPad from '@/components/Money/NumberPad.vue'
+  import Types from '@/components/Money/Types.vue'
+  import Notes from '@/components/Money/Notes.vue'
+  import Tags from '@/components/Money/Tags.vue'
+
   export default {
     name: 'Money',
+    data() {
+      return {
+        tags: ['衣', '食', '住', '行','彩票']
+      }
+    },
     components: {Tags, Notes, Types, NumberPad},
-  };
+  }
 </script>
 <style lang="scss">
-  .layout-content{
+  .layout-content {
     display: flex;
     flex-direction: column-reverse;
   }
