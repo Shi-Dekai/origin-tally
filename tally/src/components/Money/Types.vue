7e48d270a94f459f0119d33c9116de61973735f2
shi <1727259503@qq.com>
2020-07-09 10:50:05 +0800

    收集四个组建的数据到 record

--- tally/src/components/Money/Types.vue
+++ tally/src/components/Money/Types.vue
@@ -1,11 +1,11 @@
 <template>
   <div>
     <ul class="types">
-      <li :class="type === '-' && 'selected'"
-          @click="selectType('-')">收入
+      <li :class="value === '-' && 'selected'"
+          @click="selectType('-')">支出
       </li>
-      <li :class="type === '+' && 'selected'"
-          @click="selectType('+')">支出
+      <li :class="value === '+' && 'selected'"
+          @click="selectType('+')">收入
       </li>
     </ul>
   </div>
@@ -13,16 +13,17 @@
 
 <script lang="ts">
   import Vue from 'vue';
-  import {Component} from 'vue-property-decorator';
+  import {Component, Prop} from 'vue-property-decorator';
 
   @Component
   export default class Types extends Vue {
-    type = '-'; //'-'表示支出，'+'表示收入
+    @Prop(String) readonly value!: string;
+
     selectType(type: string) {
       if (type !== '-' && type !== '+') {
         throw new Error('type is unknown');
       }
-      this.type = type;
+      this.$emit('update:value', type)
     }
   };
 </script>
