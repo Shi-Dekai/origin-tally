b24121e417073b4f8184b6d19305bca2d908d68a
shi <1727259503@qq.com>
2020-07-14 17:23:20 +0800

    封装 tagList 的增删改查

--- tally/src/main.ts
+++ tally/src/main.ts
@@ -1,31 +1,40 @@
-import Vue from 'vue'
-import App from './App.vue'
-import './registerServiceWorker'
-import router from './router'
-import store from './store'
+import Vue from 'vue';
+import App from './App.vue';
+import './registerServiceWorker';
+import router from './router';
+import store from './store';
 import Nav from '@/components/Nav.vue';
 import Layout from '@/components/Layout.vue';
 import Icon from '@/components/Icon.vue';
 import {tagListModel} from '@/models/tagListModel';
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
-Vue.component('Nav', Nav)
-Vue.component('Layout', Layout)
-Vue.component('Icon', Icon)
+Vue.component('Nav', Nav);
+Vue.component('Layout', Layout);
+Vue.component('Icon', Icon);
 
-window.tagList = tagListModel.fetch()
-window.createTag = (name)=>{
+window.tagList = tagListModel.fetch();
+window.findTag = (id)=>{
+  return window.tagList.filter(t => t.id === id)[0];
+}
+window.createTag = (name) => {
   const message = tagListModel.create(name);
   if (message === 'duplicated') {
     window.alert('标签名重复了');
   } else if (message === 'success') {
     window.alert('添加成功');
   }
-}
+};
+window.removeTag = (id) => {
+  return tagListModel.remove(id);
+};
+window.updateTag = (id, name) => {
+  return tagListModel.update(id,name);
+};
 
 new Vue({
   router,
   store,
   render: h => h(App)
-}).$mount('#app')
+}).$mount('#app');
