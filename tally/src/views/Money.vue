35b9db6ffd054b6b4e7838a29ed0a20d63d37370
shi <1727259503@qq.com>
2020-07-15 14:49:34 +0800

    将所有数据封装在 store 里

--- tally/src/views/Money.vue
+++ tally/src/views/Money.vue
@@ -18,13 +18,14 @@
   import FormItem from '@/components/Money/FormItem.vue';
   import Tags from '@/components/Money/Tags.vue';
   import {Component} from 'vue-property-decorator';
+  import store from '@/store/index2';
 
   @Component({
     components: {FormItem, Tags, Types, NumberPad},
   })
   export default class Money extends Vue {
-    tags = window.tagList;
-    recordList: RecordItem[] = window.recordList;
+    tags = store.tagList;
+    recordList: RecordItem[] = store.recordList;
     record: RecordItem = {
       tags: [], notes: '', type: '-', amount: 0
     };
@@ -43,7 +44,7 @@
     }
 
     saveRecord() {
-      window.createRecord(this.record);
+      store.createRecord(this.record);
     }
 
   }
